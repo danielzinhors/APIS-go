00dbcf17ece4d3b6309b93b46e9c641c7cf65097
danielzinhors <danielzinhors@gmail.com>
2023-10-12 16:59:08 -0300

    criado entity user

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/danielzinhors/APIS-go/configs"
+
 func main() {
-	config, err := 
+	config, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	println(config.DBDriver)
 }
