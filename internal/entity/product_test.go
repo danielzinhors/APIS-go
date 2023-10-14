a587f2b66436e7e26973cc73eb520a08c5a77fa2
danielzinhors <danielzinhors@gmail.com>
2023-10-14 17:40:35 -0300

    Criado test da interface do product e ajuste no price de int para float64

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -7,34 +7,34 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.NotEmpty(t, p.ID)
 	assert.Equal(t, "Product 1", p.Name)
-	assert.Equal(t, 10, p.Price)
+	assert.Equal(t, 10.0, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	p, err := NewProduct("", 10)
+	p, err := NewProduct("", 10.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrNameisRequired, err)
 }
 
 func TestProductWhenPriceIsRequired(t *testing.T) {
-	p, err := NewProduct("Product 1", 0)
+	p, err := NewProduct("Product 1", 0.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrPriceisRequired, err)
 }
 
 func TestProductWhenPriceIsInvalid(t *testing.T) {
-	p, err := NewProduct("Product 1", -10)
+	p, err := NewProduct("Product 1", -10.0)
 	assert.Nil(t, p)
 	assert.Equal(t, ErrInvalidPrice, err)
 }
 
 func TestProductValidate(t *testing.T) {
-	p, err := NewProduct("Product 1", 10)
+	p, err := NewProduct("Product 1", 10.0)
 	assert.Nil(t, err)
 	assert.NotNil(t, p)
 	assert.Nil(t, p.Validate())
